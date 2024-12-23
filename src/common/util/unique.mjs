338b9b07ae24e31cd1e64e1d2aad560df3b199be
Sanuga Kuruppu <128165236+sanuga-lakdinu-kuruppu@users.noreply.github.com>
2024-12-23 12:23:35 +0530

    booking confirmation email added

--- src/common/util/unique.mjs
+++ src/common/util/unique.mjs
@@ -1,7 +1,21 @@
+import { v4 as uuidv4 } from "uuid";
+import QRCode from "qrcode";
+
 export const generateShortUuid = () => {
   return Math.floor(10000000 + Math.random() * 90000000);
 };
 
 export const generateOtp = () => {
   return Math.floor(1000 + Math.random() * 9000);
+};
+
+export const generateTicket = () => {
+  const eTicket = uuidv4();
+  return eTicket;
+};
+
+export const generateQrString = async (eTicket) => {
+  const qrData = JSON.stringify({ eTicket });
+  const qrToken = await QRCode.toDataURL(qrData);
+  return qrToken;
 };
