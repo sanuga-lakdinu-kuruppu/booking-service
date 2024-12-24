e88a867b8b5d8f36da597e709ad066c53ef88154
Sanuga Kuruppu <128165236+sanuga-lakdinu-kuruppu@users.noreply.github.com>
2024-12-24 18:08:54 +0530

    fetching apis were completed

--- src/lostParcel/controller/lostParcelController.mjs
+++ src/lostParcel/controller/lostParcelController.mjs
@@ -8,7 +8,12 @@ import {
 import { generateShortUuid } from "../../common/util/unique.mjs";
 import { log } from "../../common/util/log.mjs";
 import { lostParcelSchema } from "../schema/lostParcelSchema.mjs";
-import { createNewLostParcel } from "../service/lostParcelService.mjs";
+import {
+  createNewLostParcel,
+  getAllLostParcels,
+  getParcelById,
+  getParcelByReferenceId,
+} from "../service/lostParcelService.mjs";
 
 const router = Router();
 
@@ -52,6 +57,90 @@ router.post(
 
       log(baseLog, "SUCCESS", {});
       return response.status(201).send(createdLostParcel);
+    } catch (error) {
+      log(baseLog, "FAILED", error.message);
+      return response.status(500).send({ error: "internal server error" });
+    }
+  }
+);
+
+router.get(`${API_PREFIX}/lost-parcels`, async (request, response) => {
+  const baseLog = request.baseLog;
+
+  try {
+    const foundParcels = await getAllLostParcels();
+
+    log(baseLog, "SUCCESS", {});
+    return response.send(foundParcels);
+  } catch (error) {
+    log(baseLog, "FAILED", error.message);
+    return response.status(500).send({ error: "internal server error" });
+  }
+});
+
+router.get(
+  `${API_PREFIX}/lost-parcels/:parcelId`,
+  param("parcelId")
+    .isNumeric()
+    .withMessage("bad request, parcelId should be a number"),
+  async (request, response) => {
+    const baseLog = request.baseLog;
+
+    try {
+      const result = validationResult(request);
+      const {
+        params: { parcelId },
+      } = request;
+      if (!result.isEmpty()) {
+        log(baseLog, "FAILED", result.errors[0]);
+        return response.status(400).send({ error: result.errors[0].msg });
+      }
+
+      const foundParcel = await getParcelById(parcelId);
+
+      if (foundParcel) {
+        log(baseLog, "SUCCESS", {});
+        return response.send(foundParcel);
+      } else {
+        log(baseLog, "FAILED", "resource not found");
+        return response.status(404).send({ error: "resource not found" });
+      }
+    } catch (error) {
+      log(baseLog, "FAILED", error.message);
+      return response.status(500).send({ error: "internal server error" });
+    }
+  }
+);
+
+router.get(
+  `${API_PREFIX}/lost-parcels/referenceId/:referenceId`,
+  param("referenceId")
+    .isString()
+    .withMessage("bad request, referenceId should be a string"),
+  async (request, response) => {
+    const baseLog = request.baseLog;
+
+    try {
+      const result = validationResult(request);
+      const {
+        params: { referenceId },
+      } = request;
+      if (!result.isEmpty()) {
+        log(baseLog, "FAILED", result.errors[0]);
+        return response.status(400).send({ error: result.errors[0].msg });
+      }
+
+      const foundParcel = await getParcelByReferenceId(referenceId);
+
+      if (foundParcel === "NO_PARCEL_FOUND") {
+        log(baseLog, "FAILED", error.message);
+        return response
+          .status(404)
+          .send({ error: "no parcel found for this reference id" });
+      }
+
+      log(baseLog, "SUCCESS", {});
+      return response.send(foundParcel);
     } catch (error) {
       log(baseLog, "FAILED", error.message);
       return response.status(500).send({ error: "internal server error" });
